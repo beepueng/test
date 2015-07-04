afde7d2b1b13d821b8274aabb4aac4134c8faef1
beepueng <bebear1994@gmail.com>
2015-07-04 15:28:11 +0700

    initial
    initial

---
+++ androidtest.js
@@ -0,0 +1,15 @@
+/*global $, jQuery*/
+/*jslint browser: true*/ /*ต้องใส่แบบนี้เสมอนะ */
+
+$(document).ready(function () { //เว้นช่องว่างแบบนี้ทุกครั้งไม่งั้น error
+    "use strict"; //ใส่อันนี้เสมอ
+    
+    $('button').click(function () {
+       
+        window.confirm("hkjvde");
+        
+    });
+});
+    
+
+    
